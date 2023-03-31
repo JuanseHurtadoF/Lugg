c3a746f47e1460ddd7afc1d350131c1569ce5c86
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2023-03-31 17:09:27 -0500

    button component fix

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -3,7 +3,7 @@ import styles from "@styles/components/button.module.scss";
 import { ButtonProps } from "@types";
 import Link from "next/link";
 
-const Button: FunctionComponent<ButtonProps> = ({ text, linkTo, external }) => {
+const Button: FunctionComponent<ButtonProps> = ({ text, linkTo }) => {
   return (
     <Link href={linkTo}>
       <div className={styles.container}>
